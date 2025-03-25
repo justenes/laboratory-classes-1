6e9058aa63a80964b0a5156916e1f0e60bdfb2a8
justenes <enes_kocakaya@icloud.com>
2025-03-25 20:24:34 +0100

    add logout

--- routing/logout.js
+++ routing/logout.js
@@ -1,7 +1,24 @@
+const { BodyMixin } = require("undici-types");
+
 // 🏗 Stwórz funkcję 'logoutRouting', która obsłuży stronę wylogowania.
+function logoutRouting(method, response){
+
 // 🏗 Ustaw odpowiedni nagłówek 'Content-Type'.
 // Podpowiedź: response.setHeader("Content-Type", "text/html");
+response.setHeader('Content-Type', "text/html");
 // 🏗 Zakończ odpowiedź HTTP po wyrenderowaniu strony.
 // Podpowiedź: return response.end();
-
+return response.end(`
+    <html>
+      <head><title>Shop – Logout</title></head>
+      <body>
+        <h1>Logout</h1>
+        <nav>
+          <a href="/">Home</a><br>
+          <a href="/kill">Logout from application</a>
+        </nav>
+      </body>
+    </html>
+  `);
+}
 // 🔧 Wyeksportuj funkcję 'logoutRouting', aby inne moduł mogły jej używać.
